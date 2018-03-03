913818627d5bbd6e98dbf40f6ca58b1e6026160f
Andy Huynh <andy4thehuynh@gmail.com>
2018-03-03 15:53:53 -0800

    ALH #2: Adds mote templating.

--- main.rb
+++ main.rb
@@ -1,12 +1,17 @@
 require 'cuba'
 require 'cuba/safe'
+require 'cuba/contrib'
+require 'mote'
 
 Cuba.plugin Cuba::Safe
+Cuba.plugin Cuba::Mote
+
+Cuba.use Rack::Session::Cookie, :secret => '__a_very_long_string__'
 
 Cuba.define do
   on get do
     on root do
-      res.write 'hi andy'
+      render("index")
     end
   end
 end
