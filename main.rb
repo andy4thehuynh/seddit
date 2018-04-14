e3f775ac9dd9a1ffa19d4eecf551c596665ae870
Andy Huynh <andy4thehuynh@gmail.com>
2018-04-14 10:36:47 -0700

    ALH #6: Gets 1357 items off your main account.

--- main.rb
+++ main.rb
@@ -28,10 +28,59 @@ Cuba.define do
       user = req.env['redd.session']
 
       if user
+        posts = []
         path = "/user/#{user.me.name}/saved"
-        saved_content = user.client.get(path)
 
-        render('home', user: user, saved_content: saved_content, helper: self)
+        first = user.client.get(path)
+
+        posts << first.body[:data][:children]
+
+        first = user.client.get(path)
+        posts << first.body[:data][:children]
+
+        second = user.client.get(path, limit: 1000, after: first.body[:data][:after])
+        posts << second.body[:data][:children]
+
+        third = user.client.get(path, limit: 1000, after: second.body[:data][:after])
+        posts << third.body[:data][:children]
+
+        third = user.client.get(path, limit: 1000, after: second.body[:data][:after])
+        posts << third.body[:data][:children]
+
+        fourth = user.client.get(path, limit: 1000, after: third.body[:data][:after])
+        posts << fourth.body[:data][:children]
+
+        fifth = user.client.get(path, limit: 1000, after: fourth.body[:data][:after])
+        posts << fifth.body[:data][:children]
+
+        sixth = user.client.get(path, limit: 1000, after: fifth.body[:data][:after])
+        posts << sixth.body[:data][:children]
+
+        seventh = user.client.get(path, limit: 1000, after: sixth.body[:data][:after])
+        posts << seventh.body[:data][:children]
+
+        eigth = user.client.get(path, limit: 1000, after: seventh.body[:data][:after])
+        posts << eigth.body[:data][:children]
+
+        ninth = user.client.get(path, limit: 1000, after: eigth.body[:data][:after])
+        posts << ninth.body[:data][:children]
+
+        tenth = user.client.get(path, limit: 1000, after: ninth.body[:data][:after])
+        posts << tenth.body[:data][:children]
+
+        eleventh = user.client.get(path, limit: 1000, after: tenth.body[:data][:after])
+        posts << eleventh.body[:data][:children]
+
+        twelve = user.client.get(path, limit: 1000, after: eleventh.body[:data][:after])
+        posts << twelve.body[:data][:children]
+
+        thirteen = user.client.get(path, limit: 1000, after: twelve.body[:data][:after])
+        posts << thirteen.body[:data][:children]
+
+        fourteenth = user.client.get(path, limit: 1000, after: thirteen.body[:data][:after])
+        posts << fourteenth.body[:data][:children]
+
+        render('home', user: user, saved_content: posts.flatten, helper: self)
       else
         render("sign_in", user: nil)
       end
